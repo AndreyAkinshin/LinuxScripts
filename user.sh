19ecd356b2af11e1ae33e61676ec146d59b6febd
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2017-05-31 08:58:12 +0300

    remove obsolete scripts

--- user.sh
+++ user.sh
@@ -4,6 +4,7 @@ initial_user() {
   # bashrc
   sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
   cp .bashrc-custom ~/.bashrc-custom
+  cp .gitconfig ~/.gitconfig
   echo "source ~/.bashrc-custom" >> ~/.bashrc
 }
 
